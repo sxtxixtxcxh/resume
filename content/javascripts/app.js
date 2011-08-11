9442c4393d5f378e0aa49d22e1a4661d4504d141
M. Auayan <sxtxixtxcxh@gmail.com>
2011-08-11 14:24:11 -0700

    fixed sticky titles for for firefox

--- content/javascripts/app.js
+++ content/javascripts/app.js
@@ -6,14 +6,16 @@ function sticky_section_header(){
       var start = $title.data('start_sticky');
       var end = $title.data('end_sticky');
       var sticky_bottom = $title.data('sticky_bottom');
+      var left = $title.data('left');
+      var width = $title.data('width') + 10;
       if( windowTop > start && windowTop < sticky_bottom ){
-        $title.css({position: 'fixed', top: 10});
+        $title.css({position: 'fixed', top: 10, left: left});
       }else if( windowTop < start ){
-        $title.css({position: '', top: '', bottom: ''});
+        $title.css({position: 'static', top: '', bottom: '', left: ''});
       }else if( windowTop > sticky_bottom ){
-        $title.css({position: 'absolute', bottom: 0, top: ''});
+        $title.css({position: 'absolute', bottom: 0, top: '', left: width});
       }else if( windowTop > end ){
-        $title.css({position: 'static', top: '', bottom: ''});
+        $title.css({position: 'static', top: '', bottom: '', left: ''});
       }
     }
   });
@@ -26,7 +28,9 @@ $(document).ready(function(){
     var start_sticky = $section.offset().top -10;
     var end_sticky = $section.offset().top + $section.height();
     var sticky_bottom = end_sticky - $title.height() - 20;
-    $title.data({start_sticky: start_sticky, end_sticky: end_sticky, sticky_bottom: sticky_bottom});
+    var left = $title.offset().left + $title.width() + 10;
+    var width = $title.width();
+    $title.data({start_sticky: start_sticky, end_sticky: end_sticky, sticky_bottom: sticky_bottom, left: left, width: width});
   });
 
   $(window).scroll(sticky_section_header);
