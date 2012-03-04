3cf99e1901dac856a9d5a13dd9de76a609baf695
M. Auayan <sxtxixtxcxh@gmail.com>
2012-03-03 21:29:00 -0800

    update rake file

--- site/Rakefile.rb
+++ site/Rakefile.rb
@@ -1,26 +1,35 @@
-SSH_USER = 'sxtxixtxcxh'
-SSH_HOST = 'rario.us'
-SSH_DIR  = '/home/sxtxixtxcxh/m.auayan.com/public'
+PROD_SSH_USER = 'sxtxixtxcxh'
+PROD_SSH_HOST = 'm.auayan.com'
+PROD_SSH_DIR  = '/home/sxtxixtxcxh/m.auayan.com/public'
+
+STAGING_SSH_USER = 'sxtxixtxcxh'
+STAGING_SSH_HOST = 'm.auayan.com'
+STAGING_SSH_DIR  = '/home/sxtxixtxcxh/m.auayan.com/public'
 
 desc "Build the website from source"
 task :build do
-  puts "## Building website"
-  status = system("middleman build --clean")
-  puts status ? "OK" : "FAILED"
+ puts "## Building website"
+ status = system("middleman build --clean")
+ puts status ? "OK" : "FAILED"
 end
 
 desc "Run the preview server at http://localhost:4567"
 task :preview do
-  system("middleman server")
+ system("middleman server --livereload")
 end
 
-desc "Deploy website via rsync"
-task :deploy do
-  puts "## Deploying website via rsync to #{SSH_HOST}"
-  status = system("rsync -avze 'ssh' --delete build/ #{SSH_USER}@#{SSH_HOST}:#{SSH_DIR}")
-  puts status ? "OK" : "FAILED"
-end
+namespace :deploy do
+ desc "Deploy website via rsync to production"
+ task :production => [:build] do
+   puts "## Deploying website via rsync to #{PROD_SSH_HOST}"
+   status = system("rsync -avze 'ssh' --delete build/ #{PROD_SSH_USER}@#{PROD_SSH_HOST}:#{PROD_SSH_DIR}")
+   puts status ? "OK" : "FAILED"
+ end
 
-desc "Build and deploy website"
-task :gen_deploy => [:build, :deploy] do
+ desc "Deploy website via rsync to staging"
+ task :staging  => [:build]do
+   puts "## Deploying website via rsync to #{STAGING_SSH_HOST}"
+   status = system("rsync -avze 'ssh' --delete build/ #{STAGING_SSH_USER}@#{STAGING_SSH_HOST}:#{STAGING_SSH_DIR}")
+   puts status ? "OK" : "FAILED"
+ end
 end
